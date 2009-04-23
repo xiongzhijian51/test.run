ffe75f2c9321043cc5f7aa4091c502d79b4897e2
SamuraiJack <nickolay8@gmail.com>
2009-04-23 15:57:10 +0400

    - some progress on test suite

--- t/010_sanity.t.js
+++ t/010_sanity.t.js
@@ -6,11 +6,41 @@ testobj.testSanity = function() {
     //==================================================================================================================================================================================
     this.diag("Test.Run sanity");
     
-    this.ok(Test.Run, "Test.Run is here");
-    this.ok(Test.Run.Result, "Test.Run.Result is here");
+    var tap = this;
     
-        
-};
+    tap.ok(Test.Run, "Test.Run is here");
+    tap.ok(Test.Run.Result, "Test.Run.Result is here");
+    tap.ok(Test.Run.Test, "Test.Run.Test is here");
+    tap.ok(Test.Run.Harness, "Test.Run.Harness is here")
+    
+    //==================================================================================================================================================================================
+    tap.diag("Test.Run.Test creation");
+    
+    var test = new Test.Run.Test({
+    	
+    	harness : Test.Run.Harness.my,
+    	
+    	run : function (t) {
+    		t.plan(4)
+    		
+    		t.pass('Pass description')
+    		t.fail('Fail description')
+    		
+    		t.ok(true, 'True is ok')
+    		t.is(null, undefined, 'Null is undefined')
+    	}
+    })
+    
+    tap.ok(test, 'Test object was created')
+    
+    
+    //==================================================================================================================================================================================
+    tap.diag("Test.Run.Test run");
+    
+    test.start();
+    
+    tap.ok(test.planned == 4, 'Plan was setuped')
+}
 
 return testobj;
 })()
