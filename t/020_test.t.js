a7e017a7eed53d41d507297eb7e82078361c5980
SamuraiJack <nickolay8@gmail.com>
2009-04-23 18:22:51 +0400

    - some progress

--- t/020_test.t.js
+++ t/020_test.t.js
@@ -34,8 +34,11 @@ testobj.testSanity = function() {
     test.start();
     
     tap.ok(test.planned == 4, 'Plan was setuped')
+    
     tap.ok(test.results.length == 5, 'Results were created')
     tap.ok(test.current == 4, 'Current index is correct')
+    tap.ok(test.diagCount == 1, 'There was 1 diagnostic message')
+    tap.ok(test.assertCount == 4, 'There was 4 assertions')
     
     tap.ok(test.results[0] instanceof Test.Run.Result.Diagnostic, 'Very 1st result is a diagnostic message')
     tap.ok(test.results[1] instanceof Test.Run.Result.Assertion, '2nd results is an assertion')
