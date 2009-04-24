a71d7df62dd9de49101136f541a256b50eda894c
SamuraiJack <nickolay8@gmail.com>
2009-04-24 17:15:01 +0400

    - initial implementation of 'Browser.Single'

--- t/020_test.t.js
+++ t/020_test.t.js
@@ -33,15 +33,51 @@ testobj.testSanity = function() {
     
     test.start();
     
-    tap.ok(test.planned == 4, 'Plan was setuped')
+    tap.ok(test.assertPlanned == 4, 'Plan was setuped')
     
     tap.ok(test.results.length == 5, 'Results were created')
-    tap.ok(test.current == 4, 'Current index is correct')
+    tap.ok(test.currentResult == 4, 'Current index is correct')
     tap.ok(test.diagCount == 1, 'There was 1 diagnostic message')
     tap.ok(test.assertCount == 4, 'There was 4 assertions')
     
     tap.ok(test.results[0] instanceof Test.Run.Result.Diagnostic, 'Very 1st result is a diagnostic message')
     tap.ok(test.results[1] instanceof Test.Run.Result.Assertion, '2nd results is an assertion')
+    
+    
+    //==================================================================================================================================================================================
+    tap.diag("No plan #1");
+    
+    var noPlanTest1 = new Test.Run.Test({
+    	
+    	harness : Test.Run.Harness.my,
+    	
+    	run : function (t) {
+    		t.diag('Diag message')
+    	}
+    })
+    
+    noPlanTest1.start();
+    
+    tap.pass('Diagnostic messages do not require plan');
+    
+    
+    //==================================================================================================================================================================================
+    tap.diag("No plan #2");
+    
+    var noPlanTest2 = new Test.Run.Test({
+    	
+    	harness : Test.Run.Harness.my,
+    	
+    	run : function (t) {
+    		t.diag('Diag message')
+    		
+    		t.pass('no plan');
+    	}
+    })
+    
+    noPlanTest2.start();
+    
+    tap.ok(noPlanTest2.failed, 'Missing plan is detecting');
 }
 
 return testobj;
